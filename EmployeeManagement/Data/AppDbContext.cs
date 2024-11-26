5d4700e9bb0f5e3f3d7509c94751148dc47fae51
TymoteuszB <Tymoteusz.91100@gmail.com>
2024-11-26 14:03:23 +0100

    Added Cors Policy

--- EmployeeManagement/Data/AppDbContext.cs
+++ EmployeeManagement/Data/AppDbContext.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using EmployeeManagement.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace EmployeeManagement.Data
 {
     public class AppDbContext : DbContext
     {
+        public DbSet<Employee> Employees { get; set; }
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
             
--- EmployeeManagement/Program.cs
+++ EmployeeManagement/Program.cs
@@ -11,7 +11,21 @@ namespace EmployeeManagement
             builder.Services.AddDbContext<AppDbContext>(
                 options => options.UseInMemoryDatabase("EmployeeDb")
                 );
+
+            builder.Services.AddCors(
+                options =>
+                {
+                    options.AddPolicy("MyCors", builder =>
+                    {
+                        builder.WithOrigins("http://localhost:4200")
+                        .AllowAnyMethod()
+                        .AllowAnyHeader();
+                    });
+                });
+
             var app = builder.Build();
+
+            app.UseCors("MyCors");
 
             app.MapGet("/", () => "Hello World!");
 
