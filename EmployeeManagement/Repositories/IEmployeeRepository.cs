e160e5f25e3686d2450e2707915f21fcb167fefd
TymoteuszB <Tymoteusz.91100@gmail.com>
2024-11-26 14:49:56 +0100

    Created Interface for employee repository

---
+++ EmployeeManagement/Repositories/IEmployeeRepository.cs
@@ -0,0 +1,10 @@
+﻿using EmployeeManagement.Models;
+
+namespace EmployeeManagement.Repositories
+{
+    public interface IEmployeeRepository
+    {
+        Task<IEnumerable<Employee>> GetAllAsync();
+        Task<Employee> GetByIdAsync(int id);
+    }
+}
